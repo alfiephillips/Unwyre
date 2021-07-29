2dfd5f9dba9ba396217dd8fc2e7bad4e26157d76
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-07-29 11:34:29 +0100

    start register

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -9,3 +9,45 @@ import {
   FieldResolver,
   Root,
 } from "type-graphql";
+
+import { MyContext } from "../types/context";
+import { User } from "../entities/User";
+import argon2 from "argon2";
+import { COOKIE_NAME, FORGET_PASSWORD_PREFIX } from "../constants";
+import { v4 } from "uuid";
+import { getConnection } from "typeorm";
+import { Resolver } from "dns";
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+@Resolver(User)
+export class UserResolver {
+  email(@Root() user: User, @Ctx() { req }: MyContext) {
+    if (req.session.userId === user.id) {
+      return user.email;
+    }
+
+    return null;
+  }
+
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { req }: MyContext
+  );
+}
